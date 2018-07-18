620ead54d4705b6e07ffd45508428d0123574275
Gary Muir <garymuir17@gmail.com>
2018-07-18 08:21:21 +0100

    adds home page

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,10 +1,18 @@
 package controllers;
 
 import db.Seeds;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.get;
 
 public class MainController {
 
     public static void main(String[] args) {
+
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
 
         Seeds.seedData();
 
@@ -12,5 +20,14 @@ public class MainController {
         ManagersController managersController = new ManagersController();
         EngineersController engineersController = new EngineersController();
         DepartmentsController departmentsController = new DepartmentsController();
+
+        get("/", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/home.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+
+
     }
 }
